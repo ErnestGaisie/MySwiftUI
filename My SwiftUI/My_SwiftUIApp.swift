2105f4109ec7fa0380a1ff8bf00aba8d9407c286
Ernest Boamah Gaisie <e.gaisie@viuhealth.com>
2023-03-08 12:44:33 +0000

    first commit

---
+++ My SwiftUI/My_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_SwiftUIApp.swift
+//  My SwiftUI
+//
+//  Created by Gaisie on 2023/3/8.
+//
+
+import SwiftUI
+
+@main
+struct My_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
